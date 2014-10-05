4ab340c2249b42208b50ac0543567293f6110483
tobiasgrott <tobiasgrott@gmail.com>
2014-10-05 17:30:41 +0200

    added Transaction

--- src/main/java/de/clicktt/ejb/DummyService.java
+++ src/main/java/de/clicktt/ejb/DummyService.java
@@ -6,7 +6,9 @@ import de.clicktt.jpa.Dummy;
 
 public class DummyService {
 	public static void save(Dummy d){
+		EM.getEm().getTransaction().begin();
 		EM.getEm().persist(d);
+		EM.getEm().getTransaction().commit();
 	}
 	public static List<Dummy> findAll(){
 		return EM.getEm().createQuery("Select d from Dummy d",Dummy.class).getResultList();
